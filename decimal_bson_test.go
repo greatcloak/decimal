2d823cd4d3e48995d36c49083e992f01aeb8d074
Ed Pelc <ed@freightchick.com>
2023-07-24 19:07:58 -0400

    fix bson encoder/decoder 	- use battle tested codec method instead of marshal/unmarshal value interfaces 	- the *Value interfaces don't work well with omitempty or null fields

--- decimal_bson_test.go
+++ decimal_bson_test.go
@@ -14,6 +14,10 @@ func TestBSONDecimal(t *testing.T) {
 		// Test both as a value and as a pointer
 		DVal     Decimal
 		DPointer *Decimal
+		// Output nothing because we don't set value and it's omitempty
+		DPointerEmpty *Decimal `bson:",omitempty"`
+		// Output a null because we don't set value and there is no omitempty
+		DPointerEmptyButNull *Decimal
 
 		// Ensure regular fields are marshalling/unmarshalling correctly
 		Name   string
@@ -34,10 +38,26 @@ func TestBSONDecimal(t *testing.T) {
 	require.NoError(t, err, "should unmarshal bson")
 
 	require.Equal(t, td.DPointer.String(), td2.DPointer.String())
+	// require.Nil(t, td.DPointerEmpty, "omitempty bson should work")
 	require.Equal(t, td.DVal.String(), td2.DVal.String())
 
 	// Make sure other non decimal and non string fields continue to marshal and unmarshal correctly
 	// ie we didn't break the registry
 	require.Equal(t, td.Name, td2.Name)
 	require.Equal(t, td.Number, td2.Number)
+
+	var td3 bson.M
+	err = bson.Unmarshal(out, &td3)
+	require.NoError(t, err)
+
+	_, isOmitemptyFieldInMap := td3["dpointerempty"]
+	require.False(t, isOmitemptyFieldInMap, "bson omitempty should not be marshalled out as null in map, it should be omitted")
+
+	_, isInMap := td3["dpointer"]
+	require.True(t, isInMap, "bson pointer decimal field without omitempty should be marshalled out as null")
+
+	dbpointerEmptyNoOmit, isInMap := td3["dpointeremptybutnull"]
+	require.True(t, isInMap, "bson pointer decimal field without omitempty should be marshalled out as null")
+	require.Nil(t, dbpointerEmptyNoOmit)
+
 }
